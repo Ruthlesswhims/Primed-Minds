3cd90ef44878917ad9eacadc36685a360f0aeb8d
Vita Shubin <vshubin@usc.edu>
2016-09-28 03:08:28 -0700

    integrating navbar with current pages

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,31 @@
-$(document).ready(function() {
-	$(".dropdown-button").dropdown();
-})
+$(document).ready(function () {
+        $(".team1").on("mouseenter", function(){
+            $(".bio1").slideDown(1000);
+        });
+
+        $(".bio1").on("mouseleave", function(){
+            $(".bio1").slideUp();
+        });
+           $(".team2").on("mouseenter", function(){
+            $(".bio2").slideDown(1000);
+        });
+
+        $(".bio2").on("mouseleave", function(){
+            $(".bio2").slideUp();
+        });
+    });
+
+(function($) {
+		$(function() {
+
+$('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      hover: true, // Activate on hover
+      belowOrigin: true, // Displays dropdown below the button
+      constrainwidth: false, 
+    }
+  );
+
+		}); // End Document Ready
+})(jQuery); // End of jQuery name space

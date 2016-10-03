54afad1b60b0ceee6543df56ff783ea51db42abc
mhuang403 <michelle@Michelles-MacBook-Air-3.local>
2016-10-02 23:28:50 -0700

    about page updates

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,18 +1,62 @@
 $(document).ready(function () {
         $(".team1").on("mouseenter", function(){
-            $(".bio1").slideDown(1000);
+            $(".bio1").slideDown(100);
         });
 
         $(".bio1").on("mouseleave", function(){
-            $(".bio1").slideUp();
+            $(".bio1").slideUp(100);
         });
            $(".team2").on("mouseenter", function(){
-            $(".bio2").slideDown(1000);
+            $(".bio2").slideDown(100);
         });
 
         $(".bio2").on("mouseleave", function(){
-            $(".bio2").slideUp();
+            $(".bio2").slideUp(100);
         });
+           $(".team3").on("mouseenter", function(){
+            $(".bio3").slideDown(100);
+        });
+
+        $(".bio3").on("mouseleave", function(){
+            $(".bio3").slideUp(100);
+        });
+
+$(".q1").click(function(){
+        $(".a1").toggle();
+    });
+$(".q1").hover(function() {
+        $(this).css('cursor','pointer');
+    });
+$(".q2").click(function(){
+        $(".a2").toggle();
+    });
+$(".q2").hover(function() {
+        $(this).css('cursor','pointer');
+    });
+$(".q3").click(function(){
+        $(".a3").toggle();
+    });
+$(".q3").hover(function() {
+        $(this).css('cursor','pointer');
+    });
+$(".q4").click(function(){
+        $(".a4").toggle();
+    });
+$(".q4").hover(function() {
+        $(this).css('cursor','pointer');
+    });
+$(".q5").click(function(){
+        $(".a5").toggle();
+    });
+$(".q5").hover(function() {
+        $(this).css('cursor','pointer');
+    });
+$(".q6").click(function(){
+        $(".a6").toggle();
+    });
+$(".q6").hover(function() {
+        $(this).css('cursor','pointer');
+    });
     });
 
 (function($) {

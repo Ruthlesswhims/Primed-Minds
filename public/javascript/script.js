9decf096f5827d05e3e6b0177df05124f893224e
Vita Shubin <vshubin@usc.edu>
2016-11-02 16:56:42 -0700

    =fixing about page styling, making consistent navbar

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,84 +1,74 @@
 $(document).ready(function () {
-
-        $(".team1").on("mouseenter", function(){
-            $(".bio1").slideDown(100);
-        });
-
-        $(".bio1").on("mouseleave", function(){
-            $(".bio1").slideUp(10);
-        });
-           $(".team2").on("mouseenter", function(){
-            $(".bio2").slideDown(100);
-        });
-
-        $(".bio2").on("mouseleave", function(){
-            $(".bio2").slideUp(10);
-        });
-           $(".team3").on("mouseenter", function(){
-            $(".bio3").slideDown(100);
-        });
-
-        $(".bio3").on("mouseleave", function(){
-            $(".bio3").slideUp(10);
-        });
-
-$(".q1").click(function(){
-        $("#a1").toggle();
+    // ABOUT PAGE - team bios 
+    // on hover, appear
+    $(".team1").on("mouseenter", function(){
+        $(".bio1").slideDown(100);
     });
-$(".q1").hover(function() {
+    $(".team2").on("mouseenter", function(){
+        $(".bio2").slideDown(100);
+    });
+    $(".team3").on("mouseenter", function(){
+        $(".bio3").slideDown(100);
+    });
+    // click to disappear / show cursor
+    $(".team1").click(function(){
+            $(".bio1").toggle();
+    });
+    $(".team1").hover(function() {
         $(this).css('cursor','pointer');
     });
-$(".q2").click(function(){
+    $(".team2").click(function(){
+        $(".bio2").toggle();
+    });
+    $(".team2").hover(function() {
+        $(this).css('cursor','pointer');
+    });
+    $(".team3").click(function(){
+        $(".bio3").toggle();
+    });
+    $(".team3").hover(function() {
+        $(this).css('cursor','pointer');
+    });
+
+    // ABOUT PAGE - FAQ 
+    $("#q1").click(function(){
+        $("#a1").toggle();
+        $('#q1-arrow').html($('#q1-arrow').text() == "&#x25B6" ? "&#x25BC" : "&#x25B6");
+    });
+    $("#q1").hover(function() {
+        $(this).css('cursor','pointer');
+    });
+    $("#q2").click(function(){
         $("#a2").toggle();
     });
-$(".q2").hover(function() {
+    $("#q2").hover(function() {
         $(this).css('cursor','pointer');
     });
-$(".q3").click(function(){
+    $("#q3").click(function(){
         $("#a3").toggle();
     });
-$(".q3").hover(function() {
+    $("#q3").hover(function() {
         $(this).css('cursor','pointer');
     });
-$(".q4").click(function(){
+    $("#q4").click(function(){
         $("#a4").toggle();
     });
-$(".q4").hover(function() {
+    $("#q4").hover(function() {
         $(this).css('cursor','pointer');
     });
-$(".q5").click(function(){
+    $("#q5").click(function(){
         $("#a5").toggle();
     });
-$(".q5").hover(function() {
+    $("#q5").hover(function() {
         $(this).css('cursor','pointer');
     });
-$(".q6").click(function(){
+    $("#q6").click(function(){
         $("#a6").toggle();
     });
-$(".q6").hover(function() {
+    $("#q6").hover(function() {
         $(this).css('cursor','pointer');
     });
-
-// $(".team1").click(function(){
-//         $(".bio1").toggle();
-//     });
-// $(".team1").hover(function() {
-//         $(this).css('cursor','pointer');
-//     });
-// $(".team2").click(function(){
-//         $(".bio2").toggle();
-//     });
-// $(".team2").hover(function() {
-//         $(this).css('cursor','pointer');
-//     });
-// $(".team3").click(function(){
-//         $(".bio3").toggle();
-//     });
-// $(".team3").hover(function() {
-//         $(this).css('cursor','pointer');
-//     });
-
-    });
+});
 
 (function($) {
 		$(function() {

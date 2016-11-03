c7864024a21842babcda579850e76d9440e90e9b
kelseyfo <kfong@guest-wireless-207-151-061-087.usc.edu>
2016-11-02 18:30:38 -0700

    Most recent updates to card exploration

--- js/card.js
+++ js/card.js
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 function createImage() {
   $('.image').interactive = true;
 };
 
-=======
-<<<<<<< HEAD
->>>>>>> origin/dev
 // Array of cards (objects).
 var cards = [];
+
+// Save 'previous' card for this image.
+var previousCard = null;
 
 // Create 7 cards & push to array of cards.
 for(var i = 0; i < 7; i++){
@@ -20,9 +19,10 @@ for(var i = 0; i < 7; i++){
     // By default, each card is invalid (red).
     valid: false,
     // At the start, cards don't have any images.
-    image1: null,
-    image2: null,
-    image3: null,
+    // image1: null,
+    // image2: null,
+    // image3: null,
+    images: [],
   }
 
     // Add temp card to array of cards.
@@ -32,31 +32,98 @@ for(var i = 0; i < 7; i++){
 // Add cards to <div id="cards"></div>
 // Format: <div id="CARD_NAME">CARD_NAME</div>
 for(var i = 0; i < cards.length; i++){
-	$('#cards').append('<div id="' + cards[i].name + '" class="card">' + cards[i].name + '</div>');
+	$('#cards').append('<div id="' + cards[i].name + '" class="card"></div>');
 }
+
+$('.card').hide();
+$('#draw1').click(function(){
+    $('#card0').show(500);
+  
+});
+
+$('#draw2').click(function(){
+    $('#card0').show(500);
+    $('#card1').show(500);
+});
+
+$('#draw3').click(function(){
+    $('#card0').show(500);
+    $('#card1').show(500);
+    $('#card2').show(500);
+});
+
+$('#draw4').click(function(){
+    $('#card0').show(500);
+    $('#card1').show(500);
+    $('#card2').show(500);
+    $('#card3').show(500);
+});
+
+$('#draw5').click(function(){
+    $('#card0').show(500);
+    $('#card1').show(500);
+    $('#card2').show(500);
+    $('#card3').show(500);
+    $('#card4').show(500);
+});
+
+$('#draw6').click(function(){
+    $('#card0').show(500);
+    $('#card1').show(500);
+    $('#card2').show(500);
+    $('#card3').show(500);
+    $('#card4').show(500);
+    $('#card5').show(500);
+});
+
+$('#draw7').click(function(){
+    $('#card0').show(500);
+    $('#card1').show(500);
+    $('#card2').show(500);
+    $('#card3').show(500);
+    $('#card4').show(500);
+    $('#card5').show(500);
+    $('#card6').show(500);
+});
+
+$('#reset').click(function(){
+  $('.card').removeClass('valid-card');
+  $('.card').removeClass('invalid-card');
+});
 
 // Wait for the page to load.		
 $(function(){
 
-	// Dragable character images.
-	$(".image").draggable();
+	// Draggable character images.
+	$(".image").draggable({containment:'#main'});
 
 	// Droppable cards.
 	$('.card').droppable({
 
           // Triggered when a character is dropped.
 	  drop: function (event, ui) {
-
             // find index of this card using findCardIndex() function created below.
             var index = findCardIndex($(this).attr('id'));
-    
-            // Using the 'index' found above, set 'image' for this card.
-            cards[index].image = ui.draggable.attr('image');
-            
-            // Validate cards setting their 'valid' key to TRUE or FALSE using function created below.
-            validateCards();
-            // Color cards based on 'valid' key using function created below.
-            colorCards();
+
+            // Check if card already has more than 1 image.
+            if (cards[index].images.length >= 3) {
+              // Card is full, reverrt the image.
+              ui.draggable.draggable('option', 'revert', true);
+            } else {
+              // Card is NOT full, disable reverting.
+              ui.draggable.draggable('option', 'revert', false);
+
+              // Using the 'index' found above, set 'image' for this card.
+              // cards[index].image = ui.draggable.attr('image');
+
+              // Using the 'index' found above, add image to 'images' array of this card.
+              cards[index].images.push(ui.draggable.attr('image'));
+              
+              // Validate cards setting their 'valid' key to TRUE or FALSE using function created below.
+              validateCards();
+              // Color cards based on 'valid' key using function created below.
+              colorCards();
+            }
           },
 	}); /* $('.card').droppable({}); */
 
@@ -85,7 +152,6 @@ function findCardIndex(cardName) {
 } /*END findCardIndex()*/
 
 
-<<<<<<< HEAD
 /*
  * Iterate through 'cards' array and set 'valid' key to TRUE or FALSE.
   * By default, set 'valid' for all cards to FALSE.
@@ -104,13 +170,22 @@ function validateCards() {
                 // 1) 'i' cannot be the same as 'j'. Comparing cards[0].letter to cards[0].letter will always be TRUE, so we dont' want that.
                 // 2) cards[i].letter cannot be NULL. If NULL, means there is no card there, so it has to be FALSE.
                 // 3) If cards[i].letter and cards[j].letter have same letter, there is a match.
-                if (i != j
-                            && cards[i].image != null 
-                            && cards[i].image == cards[j].image) {
-                          // There is a match. set 'valid' of card[i] to TRUE and end the loop, moving on to next card.
-                          cards[i].valid = true;
-                          break;
-                }
+                // if (i != j
+                //             && cards[i].image != null 
+                //             && cards[i].image == cards[j].image) {
+                //           // There is a match. set 'valid' of card[i] to TRUE and end the loop, moving on to next card.
+                //           cards[i].valid = true;
+                //           break;
+                // }
+
+                cards[i].images.forEach(function(image1){
+                  cards[j].images.forEach(function(image2){
+                    if (i != j && image1 == image2) {
+                      cards[i].valid = true;
+                      return;
+                    }
+                  });
+                });
         }
     }
 } /*END validateCards()*/
@@ -131,71 +206,3 @@ function colorCards(){
         }
     }
 } /*END colorCards()*/
-=======
-			/*
-			 * Iterate through 'cards' array and set 'valid' key to TRUE or FALSE.
- 			 * By default, set 'valid' for all cards to FALSE.
-			 * Set 'valid' is TRUE if at least one other card has the same letter.
-			 */
-			function validateCards() {
-				// First loop here using 'i'.
-				for (var i = 0; i < cards.length; i++) {
-					// By default set all 'valid' to FALSE.
-					cards[i].valid = false;
-
-					// Second loop using 'j'.
-					for (var j = 0; j < cards.length; j++) {
-
-						// IF statements checks for 3 conditions:
-						// 1) 'i' cannot be the same as 'j'. Comparing cards[0].letter to cards[0].letter will always be TRUE, so we dont' want that.
-						// 2) cards[i].letter cannot be NULL. If NULL, means there is no card there, so it has to be FALSE.
-						// 3) If cards[i].letter and cards[j].letter have same letter, there is a match.
-						if (i != j
-								&& cards[i].image != null 
-								&& cards[i].image == cards[j].image) {
-							// There is a match. set 'valid' of card[i] to TRUE and end the loop, moving on to next card.
-							cards[i].valid = true;
-							break;
-						}
-					}
-				}
-			} /*END validateCards()*/
-
-			/*
-			 * Go through each card and color them using 'invalid-card' and 'valid-card' classes defined in CSS.
-			 * If 'valid' is TRUE, remove 'invalid-card' class and add 'valid-card' class.
-			 * If 'valid' is FALSE, remove 'valid-card' class and add 'invalid-card' class.
-			 */
-			function colorCards(){
-				for (var i = 0; i < cards.length; i++) {
-					if (cards[i].valid) {
-						$('#' + cards[i].name).removeClass('invalid-card');
-						$('#' + cards[i].name).addClass('valid-card');
-					} else {
-						$('#' + cards[i].name).removeClass('valid-card');
-						$('#' + cards[i].name).addClass('invalid-card');
-					}
-				}
-			} /*END colorCards()*/
-=======
-$( function() {
-  $( ".cardDeck" ).draggable({ containment: "#main", scroll: false });
-  $( ".obj" ).draggable({ containment: "#main", scroll: false });
-} );
-
-var images = [];
-    images[0] = 'assets/blackPom.jpg';
-    images[1] = 'assets/bwPom.jpg';
-    images[2] = 'assets/bluePom.jpg';
-    images[3] = 'assets/greenPom.jpg';
-    images[4] = 'assets/orangePom.jpg';
-    images[5] = 'assets/purplePom.jpg';
-    images[6] = 'assets/redPom.jpg';
-
-function selectRandomObj() {
-    var random = Math.floor(Math.random() * 7);
-  
-    document.querySelector('#objects img').src = images[random];
-}
->>>>>>> feliped
->>>>>>> origin/dev

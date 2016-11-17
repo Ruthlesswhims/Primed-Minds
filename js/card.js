0e56adbc9593293036306e04a02530214e0a0984
Vita Shubin <vshubin@usc.edu>
2016-11-16 17:39:28 -0800

    =moving validate and color functions from .js to .html

--- js/card.js
+++ js/card.js
@@ -1,32 +1,3 @@
-// var dragAndDrop = {
-
-//     limit: 4,
-//     count: 0,
-    
-//     init: function () {
-//         this.dragula();
-//         this.eventListeners();
-//     },
-
-//     eventListeners: function () {
-//         this.dragula.on('drop', this.dropped.bind(this));
-//     },
-
-//     dragula: function () {
-//         this.dragula = dragula([document.querySelector('#images'), document.querySelector('.card')],
-//         {
-//             copy: true,
-//         });
-//     },
-    
-//     dropped: function (el) {
-//         this.count++;
-//     }
-
-// };
-
-// dragAndDrop.init();
-
 function createImage() {
   
 };
@@ -86,41 +57,41 @@ $('#reset').click(function(){
 // Wait for the page to load.		
 $(function(){
 
-	// Draggable character images.
-	$(".image").draggable({containment:'#main'});
+// 	// Draggable character images.
+// 	$(".image").draggable({containment:'#main'});
 	
 	// Droppable cards.
-	$('.card').droppable({
+// 	$('.card').droppable({
 
     
 
-    // Triggered when a character is dropped.
-	  drop: function (event, ui) {
+//     // Triggered when a character is dropped.
+// 	  drop: function (event, ui) {
             
-	    // find index of this card using findCardIndex() function created below.
-            var index = findCardIndex($(this).attr('id'));
+// 	    // find index of this card using findCardIndex() function created below.
+//             var index = findCardIndex($(this).attr('id'));
 
-            // Check if card already has more than 3 images.
-            if (cards[index].images.length >= 3) {
-              // Card is full, revert the image.
-              ui.draggable.draggable('option', 'revert', true);
-            } else {
-              // Card is NOT full, disable reverting.
-              ui.draggable.draggable('option', 'revert', false);
+//             // Check if card already has more than 3 images.
+//             if (cards[index].images.length >= 3) {
+//               // Card is full, revert the image.
+//               ui.draggable.draggable('option', 'revert', true);
+//             } else {
+//               // Card is NOT full, disable reverting.
+//               ui.draggable.draggable('option', 'revert', false);
 
-              // Using the 'index' found above, set 'image' for this card.
-              // cards[index].image = ui.draggable.attr('image');
+//               // Using the 'index' found above, set 'image' for this card.
+//               // cards[index].image = ui.draggable.attr('image');
 
-              // Using the 'index' found above, add image to 'images' array of this card.
-              cards[index].images.push(ui.draggable.attr('image'));
+//               // Using the 'index' found above, add image to 'images' array of this card.
+//               cards[index].images.push(ui.draggable.attr('image'));
               
-              // Validate cards setting their 'valid' key to TRUE or FALSE using function created below.
-              validateCards();
-              // Color cards based on 'valid' key using function created below.
-              colorCards();
-            }  
-          },
-	}); /* $('.card').droppable({}); */
+//               // Validate cards setting their 'valid' key to TRUE or FALSE using function created below.
+//               validateCards();
+//               // Color cards based on 'valid' key using function created below.
+//               colorCards();
+//             }  
+//           },
+// 	}); /* $('.card').droppable({}); */
 
 });	/* END $(function(){}); */
 
@@ -129,21 +100,21 @@ $(function(){
  * Returns -1 if card is not found.
  * Returns card index (in this case 0 - 6) if found.
  */
-function findCardIndex(cardName) {
-	// Set index to -1 (not found) by default. If found, we'll overwrite this.
-	var index = -1;
+// function findCardIndex(cardName) {
+// 	// Set index to -1 (not found) by default. If found, we'll overwrite this.
+// 	var index = -1;
 
-	// Iterate through all the cards.
-	for(var i = 0; i < cards.length; i++){
-          if (cards[i].name == cardName) {
-              // Card name found. Set index & end looop.
-              index = i;
-              break;
-          }
-	}
+// 	// Iterate through all the cards.
+// 	for(var i = 0; i < cards.length; i++){
+//           if (cards[i].name == cardName) {
+//               // Card name found. Set index & end looop.
+//               index = i;
+//               break;
+//           }
+// 	}
 
-        return index;
-} /*END findCardIndex()*/
+//         return index;
+// } /*END findCardIndex()*/
 
 
 /*
@@ -151,51 +122,51 @@ function findCardIndex(cardName) {
   * By default, set 'valid' for all cards to FALSE.
  * Set 'valid' is TRUE if at least one other card has the same letter.
  */
-function validateCards() {
-    // First loop here using 'i'.
-    for (var i = 0; i < cards.length; i++) {
-        // By default set all 'valid' to FALSE.
-        cards[i].valid = false;
+// function validateCards() {
+//     // First loop here using 'i'.
+//     for (var i = 0; i < cards.length; i++) {
+//         // By default set all 'valid' to FALSE.
+//         cards[i].valid = false;
         
-        // Second loop using 'j'.
-        for (var j = 0; j < cards.length; j++) {
+//         // Second loop using 'j'.
+//         for (var j = 0; j < cards.length; j++) {
         
-                // IF statements checks for 3 conditions:
-                // 1) 'i' cannot be the same as 'j'. Comparing cards[0].letter to cards[0].letter will always be TRUE, so we dont' want that.
-                // 2) cards[i].letter cannot be NULL. If NULL, means there is no card there, so it has to be FALSE.
-                // 3) If cards[i].letter and cards[j].letter have same letter, there is a match.
-                if (i != j && cards[i].image != null && (cards[i].image == cards[j].image)) {
-                          // There is a match. set 'valid' of card[i] to TRUE and end the loop, moving on to next card.
-                          cards[i].valid = true;
-                          break;
-                }
+//                 // IF statements checks for 3 conditions:
+//                 // 1) 'i' cannot be the same as 'j'. Comparing cards[0].letter to cards[0].letter will always be TRUE, so we dont' want that.
+//                 // 2) cards[i].letter cannot be NULL. If NULL, means there is no card there, so it has to be FALSE.
+//                 // 3) If cards[i].letter and cards[j].letter have same letter, there is a match.
+//                 // if (i != j && cards[i].image != null && (cards[i].image == cards[j].image)) {
+//                 //           // There is a match. set 'valid' of card[i] to TRUE and end the loop, moving on to next card.
+//                 //           cards[i].valid = true;
+//                 //           break;
+//                 // }
 
-                // cards[i].images.forEach(function(image1){
-                //   cards[j].images.forEach(function(image2){
-                //     if (i != j && image1 == image2) {
-                //       cards[i].valid = true;
-                //       return;
-                //     }
-                //   });
-                // });
-        }
-    }
-} /*END validateCards()*/
+//                 cards[i].images.forEach(function(image1){
+//                   cards[j].images.forEach(function(image2){
+//                     if (i != j && image1 == image2) {
+//                       cards[i].valid = true;
+//                       return;
+//                     }
+//                   });
+//                 });
+//         }
+//     }
+// } /*END validateCards()*/
 
 /*
  * Go through each card and color them using 'invalid-card' and 'valid-card' classes defined in CSS.
  * If 'valid' is TRUE, remove 'invalid-card' class and add 'valid-card' class.
  * If 'valid' is FALSE, remove 'valid-card' class and add 'invalid-card' class.
  */
-function colorCards(){
-    for (var i = 0; i < cards.length; i++) {
-        if (cards[i].valid) {
-                // $('#' + cards[i].name).removeClass('invalid-card');
-                // $('#' + cards[i].name).addClass('valid-card');
-                cards[i].style.backgroundColor = '#5fb962';
-        } else {
-                // $('#' + cards[i].name).removeClass('valid-card');
-                // $('#' + cards[i].name).addClass('invalid-card');
-        }
-    }
-} /*END colorCards()*/
+// function colorCards(){
+//     for (var i = 0; i < cards.length; i++) {
+//         if (cards[i].valid) {
+//                 // $('#' + cards[i].name).removeClass('invalid-card');
+//                 // $('#' + cards[i].name).addClass('valid-card');
+//                 cards[i].style.backgroundColor = '#5fb962';
+//         } else {
+//                 // $('#' + cards[i].name).removeClass('valid-card');
+//                 // $('#' + cards[i].name).addClass('invalid-card');
+//         }
+//     }
+// } /*END colorCards()*/

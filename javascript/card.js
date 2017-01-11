84d5779a009e2b11a8506312c1b88b7ec2baeb68
Vita Shubin <vshubin@usc.edu>
2017-01-11 01:07:10 -0800

    =styling cleanup

--- javascript/card.js
+++ javascript/card.js
@@ -1,8 +1,8 @@
+$(document).ready(function () {
+
 // Add cards to <div id="cards"></div>
-// Format: <div id="CARD_NAME">CARD_NAME</div>
 for (var i = 0; i < 8; i++) {
     $('#cards').append('<div id="' + 'card' + i + '" class="card" ondrop="drop(event)" ondragover="allowDrop(event)"><img class="handle" src="assets/handle.png"></span></div>');
-    // $('#cards').append('<div id="' + cards[i].name + '" class="card"></div>');
 }
 
 var cardCount = 0;
@@ -27,13 +27,6 @@ $(function() {
     $(".sortable").sortable({ handle: '.handle' });
     $(".sortable").disableSelection();
 });
-
-
-
-
-
-
-//////////   test    ////
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -205,134 +198,4 @@ function colorCards() {
 }
 
 
-
-
-////// UI Interactions ///////
-
-
-
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.ENDED) {
-        console.log('ended')
-    }
-}
-
-
-$(function() {
-    $(".meter > span").each(function() {
-        $(this)
-            .data("origWidth", $(this).width())
-            .width(0)
-            .animate({
-                width: $(this).data("origWidth")
-            }, 1200);
-    });
 });
-
-//slider
-var neutralWidth = 0;
-var currentIndex = 0,
-    items = $('#main #slide'),
-    itemAmt = items.length;
-
-function cycleItems() {
-    var item = $('#main #slide').eq(currentIndex);
-    items.hide();
-    item.css('display', 'inline-block');
-}
-
-function slideIncrement(n) {
-
-    if (!n) {
-        currentIndex += 1;
-        if (currentIndex < 5) {
-            cycleItems();
-            $('#increment').animate(({
-                'width': '+=' + 20 + '%'
-            }));
-        } else {
-            currentIndex = 4;
-        }
-    } else {
-        currentIndex += n;
-        if (currentIndex < 5) {
-            cycleItems();
-            $('#increment').animate(({
-                'width': '+=' + 20 * n + '%'
-            }));
-        } else {
-            currentIndex = 4;
-        }
-    }
-
-    // if (currentIndex < 5) {
-    //     cycleItems();
-    //     $('#increment').animate(({
-    //         'width': '+=' + 20 + '%'
-    //     }));
-    // } else {
-    //     currentIndex = 4;
-    // }
-
-}
-
-function slideDecrement(n) {
-    if (!n) {
-        currentIndex -= 1;
-        if (currentIndex >= 0) {
-            $('#increment').animate(({
-                'width': '-=' + 20 + '%'
-            }))
-            cycleItems();
-        } else {
-            currentIndex = 0
-        }
-
-    } else {
-        currentIndex -= n;
-        if (currentIndex >= 0) {
-            $('#increment').animate(({
-                'width': '-=' + 20 * n + '%'
-            }))
-            cycleItems();
-        } else {
-            currentIndex = 0
-        }
-    }
-    // if (currentIndex >= 0) {
-    //     $('#increment').animate(({
-    //         'width': '-=' + 20 + '%'
-    //     }))
-    //     cycleItems();
-    // } else {
-    //     currentIndex = 0
-    // }
-}
-
-$('.right-arrow').on('click', function() {
-    slideIncrement();
-    $('.left-arrow').show();
-});
-
-$('.left-arrow').on('click', function() {
-    slideDecrement();
-});
-
-
-
-var $lengthOfProgress = $('.progress-tooltip ul li');
-
-$lengthOfProgress.on('click', function() {
-    var $this = $(this);
-    var newIndex = 0;
-    if ($this.index() > currentIndex) {
-        newIndex = $this.index() - currentIndex;
-        slideIncrement(newIndex);
-    } else {
-        newIndex = currentIndex - $this.index();
-        slideDecrement(newIndex);
-    }
-
-    cycleItems();
-
-})

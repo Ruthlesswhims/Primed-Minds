ca71f7ff438dcbbedeebaa0f07aef9b02b297538
Vita Shubin <vshubin@usc.edu>
2017-01-11 01:35:54 -0800

    =resources page code cleanup

---
+++ javascript/resources.js
@@ -0,0 +1,40 @@
+(function() { //document ready, init one or more lightbox divs
+        boomLightBoxInit([{
+            selector: 'divStatic',
+            configs: {
+                width: '775px',
+                height: '460px'
+            }
+        }, {
+            selector: 'divResponsive',
+            configs: {
+                width: '775px',
+                height: '460px'
+            }
+        }, {
+            selector: 'divResponsive2',
+            configs: {
+                width: '775px',
+                height: '460px'
+            }
+        }, {
+            selector: 'divResponsive3',
+            configs: {
+                width: '775px',
+                height: '460px'
+            }
+        }, {
+            selector: 'divResponsive4',
+            configs: {
+                width: '775px',
+                height: '460px'
+            }
+        }, {
+            selector: 'divResponsive5',
+            configs: {
+                width: '775px',
+                height: '460px'
+            }
+        }
+    ]);
+})();

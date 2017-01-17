df093aec586ba4c45a52a0f996aaa3ed241377d4
Felipe Dominguez <felipedominguez@felipes-mbp.attlocal.net>
2017-01-16 20:00:10 -0800

    jan-16 website look

--- javascript/resources.js
+++ javascript/resources.js
@@ -1,40 +1,34 @@
-(function() { //document ready, init one or more lightbox divs
-        boomLightBoxInit([{
-            selector: 'divStatic',
-            configs: {
-                width: '775px',
-                height: '460px'
-            }
-        }, {
-            selector: 'divResponsive',
-            configs: {
-                width: '775px',
-                height: '460px'
-            }
-        }, {
-            selector: 'divResponsive2',
-            configs: {
-                width: '775px',
-                height: '460px'
-            }
-        }, {
-            selector: 'divResponsive3',
-            configs: {
-                width: '775px',
-                height: '460px'
-            }
-        }, {
-            selector: 'divResponsive4',
-            configs: {
-                width: '775px',
-                height: '460px'
-            }
-        }, {
-            selector: 'divResponsive5',
-            configs: {
-                width: '775px',
-                height: '460px'
-            }
-        }
-    ]);
-})();
+$(document).ready(function() {
+    $(".team1").on("mouseenter", function() {
+        $(".bio1").slideDown(100);
+    });
+    $(".team2").on("mouseenter", function() {
+        $(".bio2").slideDown(100);
+    });
+    $(".team3").on("mouseenter", function() {
+        $(".bio3").slideDown(100);
+    });
+    // move mouse to disappear / show cursor
+    $(".col-1-3").on("mouseleave", function() {
+        $(".bio1").slideUp(100);
+    });
+    $(".team1").hover(function() {
+        $(this).css('cursor', 'pointer');
+    });
+    $(".col-2-3").on("mouseleave", function() {
+        $(".bio2").slideUp(100);
+    });
+    $(".team2").hover(function() {
+        $(this).css('cursor', 'pointer');
+    });
+    $(".col-3-3").on("mouseleave", function() {
+        $(".bio3").slideUp(100);
+    });
+    $(".team3").hover(function() {
+        $(this).css('cursor', 'pointer');
+    });
+
+
+})
+
+console.log('jquery works')
